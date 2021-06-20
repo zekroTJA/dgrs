9543e3ca20ccabc06c8f7ad6e3cc56f18f0460f9
Ringo Hoffmann <riho@live.de>
2021-06-20 09:42:49 +0200

    update example

--- examples/basic/main.go
+++ examples/basic/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -15,10 +15,10 @@ import (
 func main() {
 	godotenv.Load()
 
-	dc, _ := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
+	session, _ := discordgo.New("Bot " + os.Getenv("DISCORD_TOKEN"))
 
-	s, err := dgrc.New(dgrc.Options{
-		DiscordSession: dc,
+	state, err := dgrc.New(dgrc.Options{
+		DiscordSession: session,
 		RedisOptions: redis.Options{
 			Addr: "localhost:6379",
 		},
@@ -26,28 +26,43 @@ func main() {
 		FlushOnStartup: true,
 	})
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
-	dc.AddHandler(func(_ *discordgo.Session, e *discordgo.Ready) {
-		fmt.Println("Guilds:")
-		guilds, err := s.Guilds()
-		if err != nil {
-			fmt.Println("Err: ", err)
-			return
-		}
-		for _, g := range guilds {
-			fmt.Println(" -", g.Name)
-		}
+	session.AddHandler(func(_ *discordgo.Session, e *discordgo.Ready) {
+		log.Printf("Logged in as %s", e.User.String())
 	})
 
-	fmt.Println(dc.Open())
-	defer dc.Close()
+	session.AddHandler(func(_ *discordgo.Session, e *discordgo.MessageCreate) {
+		if e.Author.Bot || e.Content != "info" {
+			return
+		}
 
-	// fmt.Println(s.Guild("526196711962705925"))
-	// fmt.Println(s.Guilds())
-	// fmt.Println(s.Member("526196711962705925", "221905671296253953"))
+		guild, err := state.Guild(e.GuildID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		channel, err := state.Channel(e.ChannelID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		member, err := state.Member(e.GuildID, e.Author.ID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Printf("Guild: %+v\n", guild)
+		log.Printf("Channel: %+v\n", channel)
+		log.Printf("Member: %+v\n", member)
+	})
+
+	err = session.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer session.Close()
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)

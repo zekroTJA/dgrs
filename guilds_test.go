4997b930a74ec2feb60c7463c6f59fd8f548e714
Ringo Hoffmann <riho@live.de>
2021-06-22 10:05:54 +0200

    add more guild endpoint tests

--- guilds_test.go
+++ guilds_test.go
@@ -2,19 +2,26 @@ package dgrs
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-redis/redis/v8"
 	"github.com/stretchr/testify/assert"
 )
 
-func testGuild() *discordgo.Guild {
+func testGuild(id ...string) *discordgo.Guild {
+	gid := "id"
+	if len(id) > 0 {
+		gid = id[0]
+	}
+
 	return &discordgo.Guild{
-		ID:   "id",
+		ID:   gid,
 		Name: "name",
 		Members: []*discordgo.Member{
 			{
-				GuildID: "memberid",
+				GuildID: gid,
 				Nick:    "nick",
 				User: &discordgo.User{
 					ID: "memberid",
@@ -97,5 +104,47 @@ func TestGuild(t *testing.T) {
 }
 
 func TestGuilds(t *testing.T) {
+	guilds := make([]*discordgo.Guild, 10)
+	state, _ := obtainInstance()
 
+	for i := range guilds {
+		g := testGuild(fmt.Sprintf("id%d", i))
+		guilds[i] = g
+		assert.Nil(t, state.SetGuild(g))
+	}
+
+	recGuilds, err := state.Guilds()
+	assert.Nil(t, err)
+
+	i := 0
+	for _, eg := range guilds {
+		found := false
+		for _, rg := range recGuilds {
+			if eg.ID == rg.ID {
+				assert.Equal(t, eg, rg)
+				i++
+				found = true
+				break
+			}
+		}
+		assert.True(t, found, "Expected guild not found in recovered guilds", eg.ID)
+	}
+	assert.Equal(t, 10, i, "Not all guilds were recovered")
+}
+
+func TestRemoveGuild(t *testing.T) {
+	state, _ := obtainInstance()
+
+	g1 := testGuild(fmt.Sprintf("id%d", 1))
+	assert.Nil(t, state.SetGuild(g1))
+	g2 := testGuild(fmt.Sprintf("id%d", 2))
+	assert.Nil(t, state.SetGuild(g2))
+
+	assert.Nil(t, state.RemoveGuild(g1.ID))
+
+	res := state.client.Get(context.Background(), state.joinKeys(KeyGuild, g1.ID))
+	assert.ErrorIs(t, res.Err(), redis.Nil)
+	res = state.client.Get(context.Background(), state.joinKeys(KeyGuild, g2.ID))
+	assert.Nil(t, res.Err())
+	assert.Equal(t, mustMarshal(g2), res.Val())
 }
